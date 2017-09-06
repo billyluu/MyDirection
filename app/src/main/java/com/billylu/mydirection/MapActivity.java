780281d8748e1fb822d9309690c2692341611076
sheng0208 <cobi90190@gmail.com>
2017-09-06 16:52:01 +0800

    update

--- app/src/main/java/com/billylu/mydirection/MapActivity.java
+++ app/src/main/java/com/billylu/mydirection/MapActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 
-import com.billylu.mydirection.model.TranslatePosition;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
